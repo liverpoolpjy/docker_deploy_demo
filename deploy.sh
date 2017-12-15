763749dbb01cd0da2546f9d9e6684d2b776d7fc1
liverpoolpjy <864812075@qq.com>
2017-12-15 16:58:04 +0800

    update network

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker-compose build
 docker-compose up -d
-docker run --rm --link demo_db:mongo -v $('pwd')/db/dump:/backup mongo bash -c 'mongorestore /backup --host mongo:27017'
+docker run --rm --link -v $('pwd')/db/dump:/backup --net dockerdeploydemo_default mongo bash -c 'mongorestore /backup --host demo_db:27017'
